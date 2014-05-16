8e78ba7972bc8097ad3746b85d3ed02e59cf044e
Mathew Cruz <jonusx@gmail.com>
2014-05-15 22:30:51 -0400

    Added unit tests

--- polishMathTests/polishMathTests.m
+++ polishMathTests/polishMathTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "MRCPolishCalculator.h"
 
 @interface polishMathTests : XCTestCase
 
@@ -26,9 +27,88 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testAddition
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    NSError *error;
+    NSString *testString = @"1 2 +";
+    NSNumber *result = [MRCPolishCalculator calculateFromString:testString error:&error];
+    XCTAssertEqual([result integerValue], 3, @"%@ should be 3, is %@", testString, result);
+    XCTAssertNil(error, @"Error should be nil, is %@", error);
+}
+
+- (void)testDivision
+{
+    NSError *error;
+    NSString *testString = @"4 2 /";
+    NSNumber *result = [MRCPolishCalculator calculateFromString:testString error:&error];
+    XCTAssertEqual([result integerValue], 2, @"%@ should be 2, is %@", testString, result);
+    XCTAssertNil(error, @"Error should be nil, is %@", error);
+}
+
+- (void)testMultiplication
+{
+    NSError *error;
+    NSString *testString = @"25 10 *";
+    NSNumber *result = [MRCPolishCalculator calculateFromString:testString error:&error];
+    XCTAssertEqual([result integerValue], 250, @"%@ should be 250, is %@", testString, result);
+    XCTAssertNil(error, @"Error should be nil, is %@", error);
+}
+
+- (void)testSubtraction
+{
+    NSError *error;
+    NSString *testString = @"13 4 -";
+    NSNumber *result = [MRCPolishCalculator calculateFromString:testString error:&error];
+    XCTAssertEqual([result integerValue], 9, @"%@ should be 9, is %@", testString, result);
+    XCTAssertNil(error, @"Error should be nil, is %@", error);
+}
+
+- (void)testMixed
+{
+    NSError *error;
+    NSString *testString = @"3 4 + 5 6 + *";
+    NSNumber *result = [MRCPolishCalculator calculateFromString:testString error:&error];
+    XCTAssertEqual([result integerValue], 77, @"%@ should be 77, is %@", testString, result);
+    XCTAssertNil(error, @"Error should be nil, is %@", error);
+}
+
+- (void)testMixedPost
+{
+    NSError *error;
+    NSString *testString = @"2 3 4 + *";
+    NSNumber *result = [MRCPolishCalculator calculateFromString:testString error:&error];
+    XCTAssertEqual([result integerValue], 14, @"%@ should be 14, is %@", testString, result);
+    XCTAssertNil(error, @"Error should be nil, is %@", error);
+}
+
+- (void)testNotEnoughArgs
+{
+    NSError *error;
+    NSString *testString = @"1 +";
+    NSNumber *result = [MRCPolishCalculator calculateFromString:testString error:&error];
+    XCTAssertNil(result, @"result should be nil, is %@", result);
+    XCTAssertNotNil(error, @"Error should not be nil, is %@", error);
+    XCTAssertEqual(error.code, 1001, @"Improper error code");
+}
+
+- (void)testTooManyArgs
+{
+    NSError *error;
+    NSString *testString = @"1 6 8 +";
+    NSNumber *result = [MRCPolishCalculator calculateFromString:testString error:&error];
+    XCTAssertNil(result, @"result should be nil, is %@", result);
+    XCTAssertNotNil(error, @"Error should not be nil, is %@", error);
+    XCTAssertEqual(error.code, 1002, @"Improper error code");
+}
+
+- (void)testImproperArgs
+{
+    NSError *error;
+    NSString *testString = @"a b +";
+    NSNumber *result = [MRCPolishCalculator calculateFromString:testString error:&error];
+    XCTAssertNil(result, @"result should be nil, is %@", result);
+    XCTAssertNotNil(error, @"Error should not be nil, is %@", error);
+    XCTAssertEqual(error.code, 1000, @"Improper error code");
 }
 
 @end
